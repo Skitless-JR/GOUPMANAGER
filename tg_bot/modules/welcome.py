fed72c437ffc70318ab617b791c463129445a5a0
Skitless-JR <76418187+Skitless-JR@users.noreply.github.com>
2021-01-14 07:59:11 +0530

    Update welcome.py

--- tg_bot/modules/welcome.py
+++ tg_bot/modules/welcome.py
@@ -641,12 +641,12 @@ def welcome_mute_help(bot: Bot, update: Update):
 # TODO: get welcome data from group butler snap
 # def __import_data__(chat_id, data):
 #     welcome = data.get('info', {}).get('rules')
-#     welcome = welcome.replace('$username', '{username}')
-#     welcome = welcome.replace('$name', '{fullname}')
-#     welcome = welcome.replace('$id', '{id}')
-#     welcome = welcome.replace('$title', '{chatname}')
-#     welcome = welcome.replace('$surname', '{lastname}')
-#     welcome = welcome.replace('$rules', '{rules}')
+#     welcome = welcome.replace('$username', 'username')
+#     welcome = welcome.replace('$name', 'fullname')
+#     welcome = welcome.replace('$id', 'id')
+#     welcome = welcome.replace('$title', 'chatname')
+#     welcome = welcome.replace('$surname', 'lastname')
+#     welcome = welcome.replace('$rules', 'rules')
 #     sql.set_custom_welcome(chat_id, welcome, sql.Types.TEXT)
 
 
